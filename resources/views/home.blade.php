276834c9535bed2c0d1ece33ee36388a8a88b20a
prianthon <prianthonsubardio@gmail.com>
2017-12-12 21:19:35 +0700

    welcome edit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,16 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Halo</div>
+                <div class="panel-heading">Dashboard</div>
+
                 <div class="panel-body">
-                    Selamat datang di Larapus2
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
                 </div>
             </div>
         </div>

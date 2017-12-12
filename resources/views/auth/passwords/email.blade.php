a126046befbb24f908809a8d57ba43db94075b53
prianthon <prianthonsubardio@gmail.com>
2017-12-13 04:57:07 +0700

    lup blade

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -14,31 +14,25 @@
                         </div>
                     @endif
 
-                    <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
+                    {!! Form::open(['url'=>'/password/email', 'class'=>'form-horizontal']) !!}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
+                            {!! Form::label('email', 'Alamat Email', ['class'=>'col-md-4 control-label']) !!}
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                                {!! Form::email('email', null, ['class'=>'form-control']) !!}
+                                {!! $errors->first('email', '<p class="help-block">:message</p>') !!}
                             </div>
                         </div>
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
+                                    <i class="fa fa-btn fa-envelope"></i> Kirim link reset password                              
                                 </button>
                             </div>
                         </div>
-                    </form>
+
+                    {!! Form::close() !!}
                 </div>
             </div>
         </div>
